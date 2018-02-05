a534af898d3ff2b5820bbe18a13f2cc8b68842e0
Mohit Karekar <karekar.mohit@gmail.com>
2018-02-05 10:54:31 +0530

    New myController

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,20 +1,22 @@
+var request = require('request');
+var url = 'http://terriblytinytales.com/test.txt';
+var o = require('../occurrence_module');
+
 exports.indexFunction = function(req, res){
   res.send('Welcome!');
 }
 
 exports.myFunction = function(req, res){
+  var number = req.query.num;
   request(url, function(err, response, body){
     if(err) throw err;
-
-    global_occ = o.occurrence(body);
-    var number = req.query.num;
-  var num_occ = [];
-  if(number <= global_occ.length){
-    num_occ = global_occ.slice(0, number);
-  } else {
-    num_occ = global_occ;
-  }
-  res.send(num_occ);
-  })
-  
+    var global_occ = o.occurrence(body);
+    var num_occ = [];
+    if(number <= global_occ.length){
+      num_occ = global_occ.slice(0, number);
+    } else {
+      num_occ = global_occ;
+    }
+    res.send(num_occ);
+  });
 }
