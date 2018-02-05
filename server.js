f18ae4c5db4a43790da1343f02c562c7fa3908f7
Mohit Karekar <karekar.mohit@gmail.com>
2018-02-05 10:51:09 +0530

    New server.js

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 var app = require('./app');
-var request = require('request');
-var url = 'http://terriblytinytales.com/test.txt';
-var o = require('./occurrence_module');
-
 app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-
-//console.log('Server is listening on port 3001');
+  //console.log('Server is listening on port 3001');
 });
