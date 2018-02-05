71ede3d63392e2193c35d8198f4c7c048a7aea88
Mohit Karekar <karekar.mohit@gmail.com>
2018-02-05 10:44:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var url = 'http://terriblytinytales.com/test.txt';
 var o = require('./occurrence_module');
 
 app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-  request(url, function(err, response, body){
-    if(err) throw err;
-    global_occ = {};
-    global_occ = o.occurrence(body);
-  })
-  //console.log('Server is listening on port 3001');
+
+//console.log('Server is listening on port 3001');
 });
